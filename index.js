aaadfc326628a67ea57d1c5d07904e9ec68004e1
Eugene <eugeneayonga@gmail.com>
2022-06-19 19:13:02 +0300

    commit tree ff78a71496648e285d7a8667151382564bc9d0cc

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
+}
+
+
 /* 
-  Add your pseudocode here
+// pseudocode
+reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
+
 
 /*
   Add written explanation of your solution here
+  I need to make an isPalindrome function that returns either true or false. n\
+  When the input string is the same forwards and backwards, I should return true. n\
+  That means that if the input string is the same after I reverse it, n\
+  I should return true. For instance, "mom" in reverse is also "mom", n\
+  and "racecar" in reverse is also "racecar", so my solution should return \n
+  true for these cases. "hi" in reverse is "ih", so my solution should return n\
+  false for this case.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +40,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
-}
+
+  console.log("Expecting: mom");
+  console.log("=>", reverseString("mom"));
+
+  console.log("Expecting: ih");
+  console.log("=>", reverseString("hi"));
+  }
 
 module.exports = isPalindrome;
